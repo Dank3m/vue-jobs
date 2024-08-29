65196d640565b5c22fc51df83f62d770e6172988
Daniel Kimolo <dankimist@gmail.com>
2024-08-29 17:26:57 +0300

    "Updated App.vue to use Vue 3 composition API, added tasks array, toggleStatus function, and new template elements"

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
-<script>
-export default{
-  data() {
-    return {
-      name: 'John Doe',
-      status: 'pending'
-  }
-}
-}
+<script setup>
+import {ref} from 'vue';
+
+    const name = ref('John Doe');
+    const status = ref('active');
+    const tasks = ref(['Task One','Task Two','Task Three']);
+
+    const toggleStatus = () => {
+      if(status.value === 'active') {
+        status.value = 'pending';
+      }
+      else if(status.value === 'pending') {
+        status.value = 'inactive';
+      }
+      else {
+        status.value = 'active';
+      }
+    }
+
 </script>
 
 <template>
@@ -14,6 +24,13 @@ export default{
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google</a> -->
+  <button @click="toggleStatus">Change Status</button>
 </template>
 
 <style scoped>
